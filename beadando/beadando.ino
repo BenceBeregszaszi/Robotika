0a858e3b308a083b85c674a4c87f12d843450cd2
Bence Beregszászi <91261932+BenceBeregszaszi@users.noreply.github.com>
2022-05-06 10:43:10 +0200

    add case to loop

--- beadando/beadando.ino
+++ beadando/beadando.ino
@@ -1,9 +1,12 @@
+#include <Adafruit_BMP085.h>
+#include <LiquidCrystal_I2C.h>
+
 #define r 9
 #define g 7
 #define b 8
 #define rotary1 6
 #define rotary2 5
-#define rotary3 
+#define rotary3 1
 #define lcd0 3
 #define lcd1 2
 #define barometerscl 3
@@ -12,10 +15,11 @@
 #define waterlsensor 20
 #define rht 18
 #define ldr 19
+int i;
+int temp;
 
 void setup() {
 Serial.begin(9600);
-pinMode(btn, INPUT);
 pinMode(r, OUTPUT);
 pinMode(g, OUTPUT);
 pinMode(b, OUTPUT);
@@ -33,5 +37,17 @@ pinMode(ldr, INPUT);
 }
 
 void loop() {
-
+  i = map(temp, , , 1, 5)
+  switch(i){
+    case 1:
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+    case 4:
+      break;
+    case 5:
+      break;
+  }
 }
