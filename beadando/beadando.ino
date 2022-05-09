3397a91a7d92744fbd2d4f8f18c44c89d3e3c629
Bence Beregszászi <91261932+BenceBeregszaszi@users.noreply.github.com>
2022-05-09 13:18:16 +0200

    Add sensores

--- beadando/beadando.ino
+++ beadando/beadando.ino
@@ -1,12 +1,14 @@
+
 #include <Adafruit_BMP085.h>
 #include <LiquidCrystal_I2C.h>
 
+#define sealevel 1013.25
 #define r 9
 #define g 7
 #define b 8
-#define rotary1 6
-#define rotary2 5
-#define rotary3 1
+#define rotary_dt 6
+#define rotary_clk 5
+#define rotary_sw 16
 #define lcd0 3
 #define lcd1 2
 #define barometerscl 3
@@ -16,16 +18,26 @@
 #define rht 18
 #define ldr 19
 int i;
+int currentCLK;
+int lastCLK;
+unsigned long lastbutton;
 int temp;
+int btn_value;
+int btn_on;
+float bmp_temp;
+float bmp_pressure;
+float bmp_altitude;
+Adafruit_BMP085 bmp;
+
 
 void setup() {
 Serial.begin(9600);
 pinMode(r, OUTPUT);
 pinMode(g, OUTPUT);
 pinMode(b, OUTPUT);
-pinMode(rotary1, INPUT);
-pinMode(rotary2, INPUT);
-pinMode(rotary3, INPUT);
+pinMode(rotary_dt, INPUT);
+pinMode(rotary_clk, INPUT);
+pinMode(rotary_sw, INPUT_PULLUP);
 pinMode(lcd0, OUTPUT);
 pinMode(lcd1, OUTPUT);
 pinMode(barometerscl, INPUT);
@@ -34,20 +46,48 @@ pinMode(speaker, OUTPUT);
 pinMode(waterlsensor, INPUT);
 pinMode(rht, INPUT);
 pinMode(ldr, INPUT);
+lastCLK = digitalRead(rotary_clk);
+btn_on = 0;
+i = 1;
 }
 
 void loop() {
-  i = map(temp, , , 1, 5)
-  switch(i){
+  currentCLK = digitalRead(rotary_clk);
+  if(currentCLK != lastCLK && currentCLK == 1){
+    if(digitalRead(rotary_dt) != currentCLK){
+     i --;
+    }
+    else {
+      i ++;
+    }
+    Serial.print(i);
+  }
+  lastCLK = currentCLK;
+  btn_value = digitalRead(rotary_sw);
+  if(btn_value == LOW && btn_on == 0){
+    btn_on = 1;
+  }
+  if(btn_value == LOW && btn_on == 1){
+    btn_on = 0;
+  }
+  if(btn_on == 1){
+    switch(i){
     case 1:
+      bmp_temp = bmp.readTemperature();
       break;
     case 2:
+      bmp_pressure = bmp.readPressure();
       break;
     case 3:
+      bmp_altitude = bmp.readAltitude();
       break;
     case 4:
       break;
     case 5:
+      ldr_light = analogRead(ldr);
       break;
-  }
+    case 6:
+      break;
+  }  
+  } 
 }
